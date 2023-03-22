8b8114ceef7da637a31e9b954ba3358c13d1f53d
saisena1998 <saisena314@gmail.com>
2023-03-21 20:09:43 -0400

    Publish

---
+++ src/main/java/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package repository;
+import java.util.ArrayList;
+import java.util.List;
+import edu.iu.p565.customerservice.model.Customer;
+
+public class CustomerRepository {
+    private List<Customer> repository = new ArrayList<>();
+    public List<Customer>findAll(){
+        return repository;
+    }
+    
+}
